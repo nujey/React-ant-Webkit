87cad7541677524fdbd1f6971a5076d614800959
张锋 <zhangfeng@xkeshi.com>
2018-05-01 23:16:39 +0800

    合作事宜

--- src/pages/cooperation/coopera-list.js
+++ src/pages/cooperation/coopera-list.js
@@ -1,1 +1,21 @@
+
+let Coopera = {
+  lists: [{
+    pid: 1,
+    name: '电脑官网',
+    desc: '描述1',
+    mealA: true,
+    mealB: false,
+    mealC: false,
+  }, {
+    pid: 2,
+    name: '移动官网',
+    desc: '描述2',
+    mealA: true,
+    mealB: false,
+    mealC: false,
+  }]
+}
+
 // module export
+module.exports  = Coopera
