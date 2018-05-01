f202481c65d088deb72064f5b4c6c58a751a9f2e
张锋 <zhangfeng@xkeshi.com>
2018-05-01 22:56:41 +0800

    合作事宜

---
+++ src/pages/cooperation/coopera.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+import './coopera.scss'
+
+const biteStr = '合作主要是线上形式，您可以直接微信找管理人员或者是直接qq联系客服人员了解详情都是可以的，兮电为大家准备了丰富的套餐选择套餐中的每一项都是可以根据自己的需要自己搭配的，小电为大家在专业的角度划分了三个套餐供大家选择参考'
+
+class Coopera extends React.Component {
+  state = {
+    strHtml: ''
+  }
+  handleTyping = () => {
+    const arr = []
+    let num = 0
+    for(let i = 0; i < biteStr.length; i++) {
+      arr.push(biteStr[i])
+    }
+    let html = ''
+    const timer = setInterval(() => {
+      if (num < arr.length) {
+        html += arr[num]
+        this.setState({
+          strHtml: html
+        })
+        num++
+      } else {
+        clearInterval(timer)
+      }
+    }, 100)
+  }
+  render() {
+    return (
+      <div>
+        <header className="home-header">
+          <span>兮电首页</span>
+          <span>商务案例</span>
+          <a className="logo"><i className="iconfont xd-shandian"></i></a>
+          {/* <Link to="/coopera" className="nav-to-menu"><span>合作共赢</span></Link> */}
+          <span onClick={this.handleTyping}>合作共赢</span>
+          <span>联系小电</span>  
+        </header>
+        <p className="coopera">
+          {this.state.strHtml}
+        </p> 
+      </div>
+    )
+  }
+}
+export default Coopera
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Slider from "./components/sider";
 import HomeIntro from "./components/home-intro";
 import Banner from './components/banner'
-
+import { Link } from 'react-router-dom'
 // import { saveFormData } from '../../store/home/action'
 import './home.scss'
 
@@ -14,8 +14,9 @@ class Home extends React.Component {
         <header className="home-header">
           <span>兮电首页</span>
           <span>商务案例</span>
-          <a className="logo">兮电之家</a>
-          <span>合作共赢</span>
+          <a className="logo"><i className="iconfont xd-shandian"></i></a>
+          <Link to="/coopera" className="nav-to-menu"><span>合作共赢</span></Link>
+          {/* <span>合作共赢</span> */}
           <span>联系小电</span>  
         </header>
         <div>
