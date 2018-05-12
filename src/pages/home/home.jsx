0c26f2d29b52a12ba355c700d4256d3c4e638d13
张锋 <zhangfeng@xkeshi.com>
2018-05-12 21:43:52 +0800

    footer

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import Slider from "./components/sider";
+// import Slider from "./components/sider";
 import HomeIntro from "./components/home-intro";
 import Banner from './components/banner'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 // import { saveFormData } from '../../store/home/action'
-import Header from '../../components/header'
+import Header from '../../components/header/header'
 
 import './home.scss'
+import Footer from '../../components/footer/footer';
 
 class Home extends React.Component {
   render() {
@@ -20,6 +21,7 @@ class Home extends React.Component {
         <div className="home-intro clearfix">
           <HomeIntro />
         </div>
+        <Footer />
       </main>
     )
   }
